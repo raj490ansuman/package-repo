1aa8654e1490f46f489b46a258a8b8827b5b52f9
rajansuman490 <anshumanraj490@gmail.com>
2024-10-22 15:34:45 +0900

    build added

---
+++ MyComponent.js
@@ -0,0 +1,13 @@
+// src/MyComponent.js
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Hello from My Frontend Package!</h2>
+      <p>This is a reusable component you can use in your projects.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ dist/MyComponent.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+// src/MyComponent.js
+
+var MyComponent = function MyComponent() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h2", null, "Hello from My Frontend Package!"), /*#__PURE__*/_react["default"].createElement("p", null, "This is a reusable component you can use in your projects."));
+};
+var _default = exports["default"] = MyComponent;
